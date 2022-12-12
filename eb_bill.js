a93f229509f743edddfd0171c8fdc870b462fffe
Selva Ganapathi <60595640+selva1011@users.noreply.github.com>
2022-12-12 11:44:50 +0530

    Add files via upload

---
+++ eb_bill.js
@@ -0,0 +1,46 @@
+let reading_value = 610;               // you should enter your reading value in here
+var amount, addtional, sGst, cGst, Total;     // variables
+function calculate(x){                 // in this function is made for amount to calculate based on readings
+    if(x <= 150){
+        amount = x * 1.5
+        console.log(amount)   
+    }
+    else if(x <= 200){
+        addtional = x - 150
+        amount = addtional*2
+        console.log(amount)
+    }
+    else if(x <= 300){
+        addtional = x - 200
+        amount =  addtional*3 + 100
+        console.log(amount)
+    }
+    else if(x <= 600){
+        addtional = x - 300
+        amount = addtional*4.5 + 400 
+        console.log(amount)
+    }
+    else{
+        addtional = x - 600
+        amount = addtional*8 + 1750
+        console.log(amount)
+    }
+}
+function gst(x){
+    cGst = amount*.03
+    sGst = amount*.03
+    //console.log(cGst + " " + sGst)
+}
+calculate(reading_value);
+gst(amount);
+if (reading_value <= 150){             //government supsidairy for 150 units
+    amount = 0
+}
+Total = amount + sGst + cGst;
+function printout(){
+    console.log("Amount Rs " + amount)
+    console.log("sGSt Rs " + sGst)
+    console.log("cGSt Rs " +  cGst )
+    console.log("Total Rs " + Total)
+}
+printout();
